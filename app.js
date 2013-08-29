2275192b2d676f404e83ea553f9fa9aff68c1f2c
Semeon <semeon@my.office>
2013-08-29 15:21:37 +0400

    login form

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/admin', routes.admin);
+app.get('/login', routes.login);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,25 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Rock-n-roll Racing' });
+  res.render('container', 
+  					{ title: 'Rock-n-roll Racing',
+  						page: 'index'
+  					});
 };
 
-exports.admin = function(req, res){
-  res.render('admin', { title: 'Administration' });
+exports.login = function(req, res){
+  res.render('container', 
+  					{ title: 'Rock-n-roll Racing',
+  						page: 'login'
+  					});
 };
+
+
+
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Rock-n-roll Racing' });
+// };
+
+// exports.admin = function(req, res){
+//   res.render('admin', { title: 'Administration' });
+// };
