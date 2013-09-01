21aa579be3ad1a3292a21ba70fb76323e0ca5433
Semeon <semeon@my.home>
2013-09-01 12:32:54 +0400

    drivers are read from db

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,7 @@
+// Database
+var db = require('../modules/database');
+
+
 exports.index = function(req, res){
   res.render('admin', 
   					{ title: 'Admin',
@@ -7,30 +11,14 @@ exports.index = function(req, res){
 };
 
 exports.drivers = function(req, res){
-
-  var data = [];
-
-  data[0] = {};
-  data[0].id = "pup";
-  data[0].nick = "pup";
-  data[0].name = "Basil Pupkin";
-  data[0].email = "pup@mail.ru";
-  data[0].tel = "+7 921 555 5555";
-
-  data[1] = {};
-  data[1].id = "pup2";
-  data[1].nick = "pup2";
-  data[1].name = "Basil Pupkin 2";
-  data[1].email = "pup2@mail.ru";
-  data[1].tel = "+7 921 555 5555";
-
-  console.log(data);
-
-  res.render('admin', 
-            { title: 'Admin/Drivers',
-              page: 'drivers',
-              user: req.user,
-              data: data
-            });
+  var renderer = function(result) {
+    res.render( 'admin', 
+                { title: 'Admin/Drivers',
+                  page: 'drivers',
+                  user: req.user,
+                  data: result
+                });
+  }
+  db.getDrivers(renderer);
 };
 
