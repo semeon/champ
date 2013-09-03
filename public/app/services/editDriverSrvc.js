957832836b788b6eda227eaadbedcdb9e8a85a25
Semeon <semeon@my.office>
2013-09-03 18:11:09 +0400

    service updated

--- public/app/services/editDriverSrvc.js
+++ public/app/services/editDriverSrvc.js
@@ -1,4 +1,4 @@
-appModule.factory('EditDriverSrvc', ['$rootScope', '$http', function($rootScope, $http) {
+appModule.factory('EditDriverSrvc', ['$rootScope', '$http', '$compile', function($rootScope, $http, $compile) {
   var log_ctrl = ' - EditDriverSrvc: ';
   console.log('');
   console.log('EditDriver Servic started');
@@ -7,12 +7,25 @@ appModule.factory('EditDriverSrvc', ['$rootScope', '$http', function($rootScope,
   $http.get('/templates/driverForm').success(function(data) {
     driverFormHtml = data;
     // console.log(data);
-  })
+  });
+
+
 
   var editDriverService = {};
 
   editDriverService.getFormTemplate = function() {
     return driverFormHtml;
+  }
+
+  editDriverService.showDialog = function(driver) {
+    var dialogScope = $rootScope.$new(true);
+
+    dialogScope.driver = driver;
+
+    console.log(driver);
+
+    var dialogDom = $compile(driverFormHtml)(dialogScope);
+    $(dialogDom).modal();
   }
 
 
