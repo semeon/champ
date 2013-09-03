8dbd5bb4a0e2217ed95cf2d4a67b337c2d2a777d
Semeon <semeon@my.office>
2013-09-03 18:11:36 +0400

    ctrl updated - view form

--- public/app/controllers/driverListCtrl.js
+++ public/app/controllers/driverListCtrl.js
@@ -11,30 +11,7 @@ function driverListCtrl($scope, EditDriverSrvc) {
 
 		$scope.editDriverClick = function (driver) {
 
-			var template = EditDriverSrvc.getFormTemplate();
-
-			bootbox.dialog({
-			  message: template,
-			  title: "Редактировать: Имя / Ник",
-			  onEscape: function() {},
-			  show: true,
-			  backdrop: true,
-			  closeButton: true,
-			  animate: true,
-			  className: "my-modal",
-			  buttons: {
-			    saveChanges: {   
-			      label: "Сохранить",
-			      className: "btn-success",
-			      callback: function() {}
-			    },
-			    cancel: {   
-			      label: "Отменить",
-			      className: "btn-default",
-			      callback: function() {}
-			    }
-			  }
-			});
+			EditDriverSrvc.showDialog(driver);
 
 		}
 
