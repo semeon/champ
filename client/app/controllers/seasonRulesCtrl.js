5dca764fc60298e4854a0a18a1bac166120cd5cb
Semeon <semeon@my.home>
2013-09-06 21:53:35 +0400

    season rules controller

---
+++ client/app/controllers/seasonRulesCtrl.js
@@ -0,0 +1,12 @@
+function seasonRulesCtrl($scope, $rootScope, $compile, EditDataSrvc) {
+    var log_ctrl = ' - seasonRulesCtrl: ';
+    console.log('');
+    console.log('seasonRulesCtrl Controller started');
+    $scope.dataType = '';
+
+
+}
+
+
+
+
