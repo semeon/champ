3d89e5ea3b193c9f3a38f3cd8ae7fc805b98fe7f
Semeon <semeon@my.office>
2013-09-17 16:53:05 +0400

    Season deleting check

--- client/app/controllers/itemListCtrl.js
+++ client/app/controllers/itemListCtrl.js
@@ -40,33 +40,7 @@ function itemListCtrl($scope, $rootScope, $compile, EditDataSrvc, GetDataSrvc) {
 		}
 
 
-	// Public members
-	// -------------------------
-		$scope.init = function(data, type) {
-			console.log(log_ctrl + 'init()');
-			$scope.items = data;
-			$scope.dataType = String(type).toLowerCase();
-
-			console.log(log_ctrl + '$scope.dataType: ' + $scope.dataType);
-
-			if ($scope.dataType == 'races_grouped') {
-				for (var i = 0; i < $scope.items.length; i++) {
-					var item = $scope.items[i];
-					loadRaces(item);	
-				};
-			}
-		};
-
-		$scope.editButtonClick = function (item) {
-			showEditDialog(item, callDataReload);
-		}
-
-		$scope.addButtonClick = function () {
-			showEditDialog(false, callDataReload);
-		}
-
-		$scope.deleteButtonClick = function (item) {
-
+		function showDeleteDialog(item) {
 			bootbox.dialog({
 				message: '<h4 class="text-danger"><i class="icon-warning-sign"></i> Внимание!</h4>' +
 						 '<p class="text-dangers"> Вы собираетесь удалить запись: ' + 
@@ -94,11 +68,71 @@ function itemListCtrl($scope, $rootScope, $compile, EditDataSrvc, GetDataSrvc) {
 			});
 		}
 
+		function showDeleteDeclineMessage(item, message) {
+			bootbox.dialog({
+				message: '<h4 class="text-danger"><i class="icon-warning-sign"></i> Действие запрещено</h4>' +
+						 '<p class="text-dangers"> Невозможно удалить запись: ' + 
+							item.name + 
+							'.<br/>Причина: ' + message + '</p>',
+				title: "Ошибка удаления записи " + item.name,
+				onEscape: function() {},
+				show: true,
+				backdrop: true,
+				closeButton: true,
+				animate: true,
+				className: "my-modal",
+				buttons: {
+					cancel: {   
+						label: "Закрыть",
+						className: "btn-primary",
+						callback: function() {}
+					}
+				}
+			});
+		}
 
+	// Public members
+	// -------------------------
+		$scope.init = function(data, type) {
+			console.log(log_ctrl + 'init()');
+			$scope.items = data;
+			$scope.dataType = String(type).toLowerCase();
 
+			console.log(log_ctrl + '$scope.dataType: ' + $scope.dataType);
+
+			if ($scope.dataType == 'races_grouped') {
+				for (var i = 0; i < $scope.items.length; i++) {
+					var item = $scope.items[i];
+					loadRaces(item);	
+				};
+			}
+		};
+
+		$scope.editButtonClick = function (item) {
+			showEditDialog(item, callDataReload);
+		}
+
+		$scope.addButtonClick = function () {
+			showEditDialog(false, callDataReload);
+		}
+
+		$scope.deleteButtonClick = function (item) {
+
+			if (item.type == 'seasons') {
+
+				GetDataSrvc.loadItems('races', 
+															{season_id: item._id}, 
+															function(races) {
+																if (races && races.length>0) {
+																	showDeleteDeclineMessage(item, 'У выбранного сезона уже созданы этапы.');
+																} else {
+																	showDeleteDialog(item);
+																}
+															});
+
+			} else {
+				showDeleteDialog(item);
+			}
+		}
 
 }
-
-
-
-
